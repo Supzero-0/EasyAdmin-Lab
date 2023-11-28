98da9042fd8a03f40caca215d87e1cb3662f150e
Supzero-0 <mat.lecanu@gmail.com>
2023-11-28 04:57:17 +0100

    Configurate User roles field

--- src/Controller/Admin/UserCrudController.php
+++ src/Controller/Admin/UserCrudController.php
@@ -5,6 +5,7 @@ namespace App\Controller\Admin;
 use App\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\EmailField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
@@ -19,6 +20,8 @@ class UserCrudController extends AbstractCrudController
 
     public function configureFields(string $pageName): iterable
     {
+        $roles = ["ROLE_USER", "ROLE_MODERATOR", "ROLE_ADMIN", "ROLE_SUPER_ADMIN"];
+
         return [
             IdField::new('id')
                 ->onlyOnIndex(),
@@ -33,6 +36,11 @@ class UserCrudController extends AbstractCrudController
                 ->renderAsSwitch(false),
             DateField::new('createdAt')
                 ->hideOnForm(),
+            ChoiceField::new('roles')
+                ->allowMultipleChoices()
+                ->setChoices(array_combine($roles, $roles))
+                ->renderExpanded()
+                ->renderAsBadges(),
         ];
     }
 }
