ad96c8483cc9781dae70ac5f6dc54218cccc87ad
Supzero-0 <mat.lecanu@gmail.com>
2023-11-28 04:08:37 +0100

    Configure CSS for Dashboard and configure fields for User

--- src/Controller/Admin/UserCrudController.php
+++ src/Controller/Admin/UserCrudController.php
@@ -4,8 +4,10 @@ namespace App\Controller\Admin;
 
 use App\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\EmailField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
 class UserCrudController extends AbstractCrudController
@@ -15,14 +17,22 @@ class UserCrudController extends AbstractCrudController
         return User::class;
     }
 
-    /*
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            IdField::new('id')
+                ->onlyOnIndex(),
+            TextField::new('fullName')
+                ->hideOnForm(),
+            TextField::new('firstName')
+                ->onlyOnForms(),
+            TextField::new('lastName')
+                ->onlyOnForms(),
+            EmailField::new('email'),
+            BooleanField::new('enabled')
+                ->renderAsSwitch(false),
+            DateField::new('createdAt')
+                ->hideOnForm(),
         ];
     }
-    */
 }
