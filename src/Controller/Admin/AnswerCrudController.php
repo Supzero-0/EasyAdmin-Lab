5298ec00dfca00a56de89c48e2083b9c705501c5
Supzero-0 <mat.lecanu@gmail.com>
2023-11-29 17:23:41 +0100

    Override template for vote in AnswerDashboard

--- src/Controller/Admin/AnswerCrudController.php
+++ src/Controller/Admin/AnswerCrudController.php
@@ -22,7 +22,8 @@ class AnswerCrudController extends AbstractCrudController
             IdField::new('id')
                 ->onlyOnIndex(),
             Field::new('answer'),
-            IntegerField::new('votes'),
+            IntegerField::new('votes')
+                ->setTemplatePath('admin/field/votes.html.twig'),
             AssociationField::new('question')
                 ->hideOnIndex(),
             AssociationField::new('answeredBy'),
